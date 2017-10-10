b16c1a56fbc9b86bf4c358d034bdcd7bd72eca21
kevingilfether <kevingilfether@gmail.com>
2017-10-10 09:58:09 -0400

    First Commit

---
+++ Access_modifiers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_modifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //access modifiers
+            //public -- can accessed from every part of the program
+            //private -- can only be accessed from inside that class
+            //protected -- can only be accessed from inside that class and descenedent classes
+            //internal -- can only be accessed from that assembly (project)
+
+
+
+        }
+    }
+}
