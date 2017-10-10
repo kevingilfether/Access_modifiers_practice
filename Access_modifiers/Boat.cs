09caf7ce0dd9f4dbade501f0cbe934b74bcb0839
kevingilfether <kevingilfether@gmail.com>
2017-10-10 10:04:26 -0400

    Started boat class

---
+++ Access_modifiers/Boat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_modifiers
+{
+    class Boat
+    {
+        //fields
+        public int numberOfPassengers;
+
+        private string name;
+        private double engineSize;
+
+        //Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        
+        //Constructors
+
+        public Boat()
+        {
+
+        }
+
+        //Methods
+
+        public void Move()
+        {
+
+        }
+        private double CalculateMPG()
+        {
+            
+        }
+
+
+    }
+}
